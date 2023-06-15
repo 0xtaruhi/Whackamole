f596bcf434646bf36caaa74c5082fbdb2857308b
0xtaruhi <zhang_zhengyi@outlook.com>
2023-06-15 12:17:45 +0800

    feat(utils): coe generate util

---
+++ utils/src/coe/coe.rs
@@ -0,0 +1,52 @@
+use std::io::Write;
+
+pub struct Coe<T> {
+    pub filename: String,
+    pub width: u32,
+    pub depth: u32,
+    pub data: Vec<T>,
+}
+
+impl<T> Coe<T>
+  where T: std::fmt::Binary + Copy
+ {
+    pub fn new(filename: &str, width: u32, depth: u32, data: Vec<T>) -> Self {
+        Coe {
+            filename: filename.to_string(),
+            width,
+            depth,
+            data,
+        }
+    }
+
+    pub fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        println!(
+            "Saving {} as a {}-bit 2-radix coe file",
+            self.filename, self.width
+        );
+
+        let mut file = std::fs::File::create(&self.filename)?;
+        file.write_all(self.get_header().as_bytes())?;
+        file.write_all(self.get_contents().as_bytes())?;
+        Ok(())
+    }
+
+    fn get_header(&self) -> String {
+        let mut header = String::new();
+        header.push_str("memory_initialization_radix=2;\n");
+        header.push_str("memory_initialization_vector=\n");
+        header
+    }
+
+    fn get_contents(&self) -> String {
+        let mut contents = String::new();
+        for (i, pixel) in self.data.iter().enumerate() {
+            contents.push_str(&format!("{:0width$b}", pixel, width = self.width as usize));
+            if i < self.data.len() - 1 {
+                contents.push_str(",\n");
+            }
+        }
+        contents.push_str(";");
+        contents
+    }
+}
---
+++ utils/src/main.rs
@@ -0,0 +1,50 @@
+mod coe;
+
+use coe::coe::Coe;
+use image::{DynamicImage, GenericImageView};
+use std::env;
+
+fn write_coe(img: &DynamicImage, coe_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let (width, height) = img.dimensions();
+    println!("Image dimensions: {}x{}", width, height);
+
+    let mut data: Vec<u16> = Vec::new();
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = img.get_pixel(x, y);
+            let r = (pixel[0] >> 3) & 0x1f;
+            let g = (pixel[1] >> 3) & 0x1f;
+            let b = (pixel[2] >> 3) & 0x1f;
+            let a = (pixel[3] >> 7) & 0x01;
+            let mut pixel: u16 = 0;
+            pixel = pixel | (a as u16) << 15;
+            pixel = pixel | (b as u16) << 10;
+            pixel = pixel | (g as u16) << 5;
+            pixel = pixel | (r as u16);
+            data.push(pixel);
+        }
+    }
+
+    let coe = Coe::new(coe_path, width, height, data);
+    coe.save()?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <image_path>", args[0]);
+        return Ok(());
+    }
+    let image_path = &args[1];
+    let mut store_path = args[2].clone();
+
+    if std::path::Path::new(&store_path).is_dir() {
+        let raw_image_name = image_path.split("/").last().unwrap().split(".").next().unwrap();
+        store_path = format!("{}/{}.coe", store_path, raw_image_name);
+    }
+
+    let img = image::open(image_path.as_str()).map_err(|e| e.to_string())?;
+    write_coe(&img, &store_path)?;
+    Ok(())
+}
